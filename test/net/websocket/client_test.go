7880b69023d7602d5e49fff9ef99a9a835c15d9a
TKOFFICE\sunbin01 <sunbin01@mail.jj.cn>
2023-06-08 11:38:51 +0800

    websocket client

--- test/net/websocket/client_test.go
+++ test/net/websocket/client_test.go
@@ -10,34 +10,52 @@ import (
 
 func TestClient(t *testing.T) {
 	var addr = "192.168.59.35:9102"
-	err := webscok.Init(addr)
+	webClient, err := webscok.Init(addr)
 	if err != nil {
 		panic("init failed")
 	}
 
-	defer webscok.GWebClient.Close()
+	defer webClient.Close()
 	var wg sync.WaitGroup
 
+	var continueFlag = true
+
+	//等待结束
+	go func() {
+		select {
+		case <-time.After(20 * time.Second):
+			continueFlag = false
+
+		}
+	}()
+
+	// 持续读取
 	go func() {
 		wg.Add(1)
 		defer wg.Done()
-		for {
-			_, _, err = webscok.GWebClient.ReadMessage()
+		for continueFlag {
+			_, _, err = webClient.ReadMessage()
 			if err != nil {
 				break
 			}
 			zaplog.LoggerSugar.Info("go runtine contine true")
 			time.Sleep(50 * time.Millisecond)
 		}
+
 	}()
 
+	// 主程序发送
 	var cgwVerify = "{\n    \"protocolId\":61001,\n    \"data\":{\n        \"userinfo\":\"123xx68\",\n        \"isreconnect\":0,\n        \"centerid\":7905\n    }\n}"
-	var cgLogin = "{\n    \"protocolId\":61012,\n    \"data\":{\n        \"GameVersion\":\"gameVersion=1.1.0&resVersion=1.1.0&patch=30\",\n        \"MaxPacketId\":564,\n        \"deviceInfo\":{\n\t\t\t\"appVersion\":\"1\"\n\t\t},\n        \"Account\":\"123xx68\",\n        \"type\":1,\n        \"token\":\"\",\n        \"uuid\":\"\"\n    }\n}\n"
+	var cgwLogin = "{\n    \"protocolId\":61012,\n    \"data\":{\n        \"GameVersion\":\"gameVersion=1.1.0&resVersion=1.1.0&patch=30\",\n        \"MaxPacketId\":564,\n        \"deviceInfo\":{\n\t\t\t\"appVersion\":\"1\"\n\t\t},\n        \"Account\":\"123xx68\",\n        \"type\":1,\n        \"token\":\"\",\n        \"uuid\":\"\"\n    }\n}\n"
 	var cpEnrollMatch = "{\n    \"protocolId\":384,\n    \"data\":{\n        \"matchId\":\"1111\",\n        \"tbId\":1\n    }\n}"
-	var msgList = []string{cgwVerify, cgLogin, cpEnrollMatch}
+	var cgwConnectGamesever = "{\n    \"protocolId\":61004,\n    \"data\":{\n        \"gameserverid\":5524,\n        \"isreconnect\":0\n    }\n}"
+	var cgLogin = "{\n    \"protocolId\":1,\n    \"data\":{\n        \"GameVersion\":0,\n        \"ProgramVersion\":108,\n        \"MaxPacketId\":243,\n        \"Account\":\"123xx68\",\n        \"sex\":0\n    }\n}"
+	var cgEnterSceneOk = "{\n    \"protocolId\":14,\n    \"data\":{\n        \"IsOK\":1\n    }\n}"
+	var msgList = []string{cgwVerify, cgwLogin, cpEnrollMatch, cgwConnectGamesever, cgLogin, cgEnterSceneOk}
 
 	for _, v := range msgList {
-		webscok.GWebClient.WriteTextMessage([]byte(v))
+		webClient.WriteTextMessage([]byte(v))
+		time.Sleep(500 * time.Millisecond)
 	}
 
 	wg.Wait()

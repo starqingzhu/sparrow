3faf8f8145e6e6127ff37384a37d7bbcf15e0b46
TKOFFICE\sunbin01 <sunbin01@mail.jj.cn>
2023-08-15 09:28:51 +0800

    性能分析引入

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
-	"sparrow/pkg/cache/redis"
+	"os"
+	"runtime"
+	"runtime/trace"
 )
 
 func main() {
+	runtime.GOMAXPROCS(4)
+	f, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	//启动trace goruntine
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+
+	defer trace.Stop()
+
+	//debug.SetMaxThreads(10)
 	fmt.Println("enter.........")
-	redis.Init()
+
+	//redis.Init()
+	//time.Sleep(100 * time.Second)
 }
